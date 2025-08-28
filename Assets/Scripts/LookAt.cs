71eeb67d9d8631609e279701e29ae5d206226769
gtanushree2111-hub <gtanushree0521@gmail.com>
2025-08-28 21:33:22 +0530

    commit

---
+++ Assets/Scripts/LookAt.cs
@@ -0,0 +1,27 @@
+using  UnityEngine;
+
+
+   public class LookAt: MonoBehaviour
+{ 
+    //target that the camera should look at
+    public static GameObject target;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    if (target == null)
+    {
+            target =this .gameObject;
+            Debug.Log("Look at target target not specified.Defaulting to parent GameObject");
+    }    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target){
+            transform.LookAt(target.transform);
+        }
+         
+    }
+  }
+ 
